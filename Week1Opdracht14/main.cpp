8d649ed9f4349306d5a92ef1524a85ef2420ef51
Marijn Beek, van <s1215221@student.windesheim.nl>
2025-09-25 17:58:52 +0200

    Opgave 14

---
+++ Week1Opdracht14/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int getal;
+	int keuze;
+
+	printf("Geef een getal: ");
+	scanf_s("%d", &getal);
+
+	printf("Maak een keuze: Wil je **2 (1) of **2 (2) ");
+	scanf_s("%d", &keuze);
+
+	if (keuze == 1) {
+		int kwadraad = (getal * getal);
+		printf("Het kwadraad van dit getal is %d", kwadraad);
+	}
+
+	if (keuze == 2) {
+		int kwadraad3 = (getal * getal * getal);
+		printf("Het getaal tot de macht 3 is %d", kwadraad3);
+	}
+
+}
