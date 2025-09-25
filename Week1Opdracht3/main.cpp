3d23b01bd3d45e2c4fd481c689fd8d1e461178b4
Marijn Beek, van <s1215221@student.windesheim.nl>
2025-09-25 16:27:05 +0200

    Opdracht 3

---
+++ Week1Opdracht3/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	printf("Geef 2 waardes: ");
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+
+	if (a > b) {
+		printf("%d is groter dan %d", a,b);
+	}
+
+	if (a < b) {
+		printf("%d is groter dan %d", b,a);
+	}
+}
